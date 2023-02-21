ea0eb5f655a736b33ad43a6ba134a3d050f11c16
jbf <jfarmeriv@gmail.com>
2023-02-21 14:23:06 -0500

    Project Creation

--- src/test/java/com/jbf/testmspipelinebuild/TestMsPipelineBuildApplicationTests.java
+++ src/test/java/com/jbf/testmspipelinebuild/TestMsPipelineBuildApplicationTests.java
@@ -1,9 +1,11 @@
 package com.jbf.testmspipelinebuild;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@SpringBootConfiguration
 class TestMsPipelineBuildApplicationTests {
 
     @Test
